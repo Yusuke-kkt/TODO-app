7cf2cc2ac12d62d99b4010b53b55de5f4cdf0af4
yusuke <byysowbach21@gmail.com>
2025-01-24 14:18:35 +0900

    2025/1/24

--- api/routers/task.py
+++ api/routers/task.py
@@ -36,3 +36,26 @@ async def delete_task(task_id: int):
 @router.get("/tasks", response_model=List[task_schema.Task])
 async def list_tasks(db: AsyncSession = Depends(get_db)):
     return await task_crud.get_tasks_with_done(db)
+
+
+from fastapi import APIRouter, Depends, HTTPException
+
+
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)
+
+
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
